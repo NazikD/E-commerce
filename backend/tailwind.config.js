427d3571f77df26dff0fedbfee5e3ec7be050778
NazikD <nazar.doroshenkov@gmail.com>
2023-06-05 11:14:52 +0300

    Create Vue and install Tailwind

---
+++ backend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './components/*',
+        './index.html',
+      ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
