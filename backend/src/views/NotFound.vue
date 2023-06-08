dc6b79463323fcb700236262cd084a13f6e3fbb6
NazikD <nazar.doroshenkov@gmail.com>
2023-06-08 12:16:04 +0300

    Create 404 page

--- backend/src/views/NotFound.vue
+++ backend/src/views/NotFound.vue
@@ -1,29 +1,26 @@
 <template>
-  <div scroll-py-5>
-    <div class="flex justify-center items-center min-h-full w-full min-w-full">
-      <div class="flex">
-        <div
-          class="text-indigo-600 self-start text-5xl px-8 font-bold border-r pb-8 leading-10"
-        >
-          404
+  <section class="bg-white dark:bg-gray-900 ">
+    <div class="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+        <div class="wf-ull lg:w-1/2">
+            <p class="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
+            <h1 class="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
+            <p class="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist.Here are some helpful links:</p>
+
+            <div class="flex items-center mt-6 gap-x-3">
+                <router-link :to="{name: 'app.dashboard'}" class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                    </svg>
+                    <span>Go back</span>
+                </router-link>
+            </div>
         </div>
-        <div class="px-8">
-          <h1 class="text-5xl font-bold mb-2">Page Not Found</h1>
-          <p class="text-gray-400 mb-10">
-            Please check the url in address bar and try again
-          </p>
-          <div>
-            <router-link
-              :to="{ name: 'app.dashboard' }"
-              class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Go back home
-            </router-link>
-          </div>
+
+        <div class="relative w-full mt-12 lg:w-1/2 lg:mt-0">
+            <img class="w-full max-w-lg lg:mx-auto" src="../images/components/illustration.svg" alt="">
         </div>
-      </div>
     </div>
-  </div>
+</section>
 </template>
 
 <script setup>
